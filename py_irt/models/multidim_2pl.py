ec726891dc051b8447410ef8719cc27706e769a1
Lorenzo Pacchiardi <lorenzo.pacchiardi@stats.ox.ac.uk>
2024-04-18 11:14:19 +0100

    Update multidim_2pl.py

--- py_irt/models/multidim_2pl.py
+++ py_irt/models/multidim_2pl.py
@@ -52,10 +52,16 @@ class Multidim2PL(IrtModel):
     def export(self):
         return {
             "ability": pyro.param("loc_ability").data.tolist(),
+            "scale_ability": pyro.param("scale_ability").data.tolist(),
             "diff": pyro.param("loc_diff").data.tolist(),
             "disc": pyro.param("loc_disc").data.tolist(),
+            "loc_mu_theta": pyro.param("loc_mu_theta").data.tolist(),
+            "scale_mu_theta": pyro.param("scale_mu_theta").data.tolist(),
+            "alpha_theta": pyro.param("alpha_theta").data.tolist(),
+            "beta_theta": pyro.param("beta_theta").data.tolist(),
         }
 
+    
     def get_model(self):
         return self.model_hierarchical
 
@@ -63,16 +69,16 @@ class Multidim2PL(IrtModel):
         return self.guide_hierarchical
 
     def model_hierarchical(self, subjects, items, obs):
-        with pyro.plate("mu_b_plate", self.dims):
+        with pyro.plate("mu_b_plate", 1):
             mu_b = pyro.sample(
                 "mu_b",
                 dist.Normal(
                     torch.tensor(0.0, device=self.device),
-                    torch.tensor(1.0e6, device=self.device),
+                    torch.tensor(1.0e1, device=self.device),
                 ),
             )
 
-        with pyro.plate("u_b_plate", self.dims):
+        with pyro.plate("u_b_plate", 1):
             u_b = pyro.sample(
                 "u_b",
                 dist.Gamma(
@@ -86,7 +92,7 @@ class Multidim2PL(IrtModel):
                 "mu_theta",
                 dist.Normal(
                     torch.tensor(0.0, device=self.device),
-                    torch.tensor(1.0e6, device=self.device),
+                    torch.tensor(1.0e1, device=self.device),
                 ),
             )
 
@@ -104,7 +110,7 @@ class Multidim2PL(IrtModel):
                 "mu_gamma",
                 dist.Normal(
                     torch.tensor(0.0, device=self.device),
-                    torch.tensor(1.0e6, device=self.device),
+                    torch.tensor(1.0e1, device=self.device),
                 ),
             )
 
@@ -122,7 +128,7 @@ class Multidim2PL(IrtModel):
                 ability = pyro.sample("theta", dist.Normal(mu_theta, 1.0 / u_theta))
 
         with pyro.plate("bs", self.num_items, dim=-2, device=self.device):
-            with pyro.plate("bs_dims", self.dims, dim=-1):
+            with pyro.plate("bs_dims", 1, dim=-1):
                 diff = pyro.sample("b", dist.Normal(mu_b, 1.0 / u_b))
 
         with pyro.plate("gammas", self.num_items, dim=-2, device=self.device):
@@ -130,41 +136,41 @@ class Multidim2PL(IrtModel):
                 disc = pyro.sample("gamma", dist.Normal(mu_gamma, 1.0 / u_gamma))
 
         with pyro.plate("observe_data", obs.size(0)):
-            multidim_logits = disc[items] * (ability[subjects] - diff[items])
+            multidim_logits = disc[items] * ability[subjects] - diff[items]
             logits = multidim_logits.sum(axis=-1)
 
             pyro.sample("obs", dist.Bernoulli(logits=logits), obs=obs)
 
     def guide_hierarchical(self, subjects, items, obs):
-        loc_mu_b_param = pyro.param("loc_mu_b", torch.zeros(self.dims, device=self.device))
+        loc_mu_b_param = pyro.param("loc_mu_b", torch.zeros(1, device=self.device))
         scale_mu_b_param = pyro.param(
             "scale_mu_b",
-            1e2 * torch.ones(self.dims, device=self.device),
+            torch.ones(1, device=self.device), #1e2 * 
             constraint=constraints.positive,
         )
 
         loc_mu_theta_param = pyro.param("loc_mu_theta", torch.zeros(self.dims, device=self.device))
         scale_mu_theta_param = pyro.param(
             "scale_mu_theta",
-            1e2 * torch.ones(self.dims, device=self.device),
+            torch.ones(self.dims, device=self.device), #1e2 * 
             constraint=constraints.positive,
         )
 
         loc_mu_gamma_param = pyro.param("loc_mu_gamma", torch.zeros(self.dims, device=self.device))
         scale_mu_gamma_param = pyro.param(
             "scale_mu_gamma",
-            1.0e2 * torch.ones(self.dims, device=self.device),
+            torch.ones(self.dims, device=self.device), #1e2 * 
             constraint=constraints.positive,
         )
 
         alpha_b_param = pyro.param(
             "alpha_b",
-            torch.ones(self.dims, device=self.device),
+            torch.ones(1, device=self.device),
             constraint=constraints.positive,
         )
         beta_b_param = pyro.param(
             "beta_b",
-            torch.ones(self.dims, device=self.device),
+            torch.ones(1, device=self.device),
             constraint=constraints.positive,
         )
 
@@ -200,11 +206,11 @@ class Multidim2PL(IrtModel):
         )
 
         m_b_param = pyro.param(
-            "loc_diff", torch.zeros([self.num_items, self.dims], device=self.device)
+            "loc_diff", torch.zeros([self.num_items, 1], device=self.device)
         )
         s_b_param = pyro.param(
             "scale_diff",
-            torch.ones([self.num_items, self.dims], device=self.device),
+            torch.ones([self.num_items, 1], device=self.device),
             constraint=constraints.positive,
         )
 
@@ -218,10 +224,10 @@ class Multidim2PL(IrtModel):
         )
 
         # sample statements
-        with pyro.plate("mu_b_plate", self.dims):
+        with pyro.plate("mu_b_plate", 1):
             mu_b = pyro.sample("mu_b", dist.Normal(loc_mu_b_param, scale_mu_b_param))
 
-        with pyro.plate("u_b_plate", self.dims):
+        with pyro.plate("u_b_plate", 1):
             u_b = pyro.sample("u_b", dist.Gamma(alpha_b_param, beta_b_param))
 
         with pyro.plate("mu_theta_plate", self.dims):
@@ -244,9 +250,11 @@ class Multidim2PL(IrtModel):
                 theta = pyro.sample("theta", dist.Normal(m_theta_param, s_theta_param))
 
         with pyro.plate("bs", self.num_items, dim=-2, device=self.device):
-            with pyro.plate("bs_dims", self.dims, dim=-1):
+            with pyro.plate("bs_dims", 1, dim=-1):
                 b = pyro.sample("b", dist.Normal(m_b_param, s_b_param))
 
         with pyro.plate("gammas", self.num_items, dim=-2, device=self.device):
             with pyro.plate("gamma_dims", self.dims, dim=-1, device=self.device):
                 gamma = pyro.sample("gamma", dist.Normal(m_gamma_param, s_gamma_param))
+
+
